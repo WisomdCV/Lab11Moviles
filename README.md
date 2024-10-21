0a759429866cc184ebe077716bbc9ca13ed08ab1
WisomdCV <wilsondcv711@gmail.com>
2024-10-21 14:23:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lab12Moviles
+ Laboratorio que dice que es 11 pero realmente es el 12.
---
+++ app/src/main/java/com/example/lab11_moviles/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.lab11_moviles
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab11_moviles.ui.theme.Lab11MovilesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MapScreen()
+        }
+    }
+}
