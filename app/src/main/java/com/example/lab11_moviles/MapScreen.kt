f8a221f2f7e88e4ae0e650b01512b25db5a483dc
WisomdCV <wilsondcv711@gmail.com>
2024-10-21 16:23:45 -0500

    Cambios de icono y dependencias.
    Primer commit con primeras modificaciones.

--- app/src/main/java/com/example/lab11_moviles/MapScreen.kt
+++ app/src/main/java/com/example/lab11_moviles/MapScreen.kt
@@ -1,25 +1,43 @@
 package com.example.lab11_moviles
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
 
-// Encargado de la gestión del mapa
+
 @Composable
 fun MapScreen() {
+    val context = LocalContext.current
     val ArequipaLocation = LatLng(-16.4040102, -71.559611) // Arequipa, Perú
     val cameraPositionState = rememberCameraPositionState {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
 
+    
+    fun bitmapDescriptorFromVector(context: Context, vectorResId: Int, size: Int): BitmapDescriptor {
+        val vectorDrawable: Drawable = ContextCompat.getDrawable(context, vectorResId)!!
+        vectorDrawable.setBounds(0, 0, size, size)
+        val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
-        // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
@@ -27,6 +45,7 @@ fun MapScreen() {
             // Añadir marcador en Arequipa, Perú
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
+                icon = bitmapDescriptorFromVector(context, R.drawable.pokeparada, 100), // Icono personalizado con tamaño ajustado
                 title = "Arequipa, Perú"
             )
         }
