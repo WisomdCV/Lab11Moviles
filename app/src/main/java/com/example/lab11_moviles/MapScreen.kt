3e1ac283cf831446fdf130d58e62dfd83ab2b060
WisomdCV <wilsondcv711@gmail.com>
2024-10-21 20:35:24 -0500

    Marcadores y Control de cámara programable
    Agregamos marcadores en ciertas ubicaciones con coordenadas, y luego de ello marcadores en los mismos, además de una cámara destinada a mostrar solo un lugar.

--- app/src/main/java/com/example/lab11_moviles/MapScreen.kt
+++ app/src/main/java/com/example/lab11_moviles/MapScreen.kt
@@ -7,9 +7,11 @@ import android.graphics.drawable.Drawable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
@@ -18,7 +20,7 @@ import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
 
-
+// Encargado de la gestión del mapa
 @Composable
 fun MapScreen() {
     val context = LocalContext.current
@@ -27,7 +29,7 @@ fun MapScreen() {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
 
-    
+    // Función para convertir un recurso de imagen en BitmapDescriptor con un tamaño ajustado
     fun bitmapDescriptorFromVector(context: Context, vectorResId: Int, size: Int): BitmapDescriptor {
         val vectorDrawable: Drawable = ContextCompat.getDrawable(context, vectorResId)!!
         vectorDrawable.setBounds(0, 0, size, size)
@@ -37,17 +39,40 @@ fun MapScreen() {
         return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
+    // Lista de ubicaciones
+    val locations = listOf(
+        LatLng(-16.433415, -71.5442652), // JLByR
+        LatLng(-16.4205151, -71.4945209), // Paucarpata
+        LatLng(-16.3524187, -71.5675994) // Zamacola
+    )
+
+    LaunchedEffect(Unit) {
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984,-71.6836503), 12f), // Mover a Yura
+            durationMs = 3000
+        )
+    }
+
+
     Box(modifier = Modifier.fillMaxSize()) {
+        // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Arequipa, Perú
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
                 icon = bitmapDescriptorFromVector(context, R.drawable.pokeparada, 100), // Icono personalizado con tamaño ajustado
                 title = "Arequipa, Perú"
             )
+
+            locations.forEach { location ->
+                Marker(
+                    state = rememberMarkerState(position = location),
+                    title = "Ubicación",
+                    snippet = "Punto de interés"
+                )
+            }
         }
     }
 }
