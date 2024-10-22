15cad5b4617ef1dcd6669f4615e136fa30af3692
WisomdCV <wilsondcv711@gmail.com>
2024-10-21 20:44:17 -0500

    Poligonos en plazas
    Realmente no estaban situados en las plazas de arequipa .-,

--- app/src/main/java/com/example/lab11_moviles/MapScreen.kt
+++ app/src/main/java/com/example/lab11_moviles/MapScreen.kt
@@ -9,15 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
-import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
 
 // Encargado de la gestión del mapa
@@ -46,13 +48,34 @@ fun MapScreen() {
         LatLng(-16.3524187, -71.5675994) // Zamacola
     )
 
+    // Polígonos
+    val mallAventuraPolygon = listOf(
+        LatLng(-16.432292, -71.509145),
+        LatLng(-16.432757, -71.509626),
+        LatLng(-16.433013, -71.509310),
+        LatLng(-16.432566, -71.508853)
+    )
+
+    val parqueLambramaniPolygon = listOf(
+        LatLng(-16.422704, -71.530830),
+        LatLng(-16.422920, -71.531340),
+        LatLng(-16.423264, -71.531110),
+        LatLng(-16.423050, -71.530600)
+    )
+
+    val plazaDeArmasPolygon = listOf(
+        LatLng(-16.398866, -71.536961),
+        LatLng(-16.398744, -71.536529),
+        LatLng(-16.399178, -71.536289),
+        LatLng(-16.399299, -71.536721)
+    )
+
     LaunchedEffect(Unit) {
         cameraPositionState.animate(
-            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984,-71.6836503), 12f), // Mover a Yura
+            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984, -71.6836503), 12f), // Mover a Yura
             durationMs = 3000
         )
     }
-
 
     Box(modifier = Modifier.fillMaxSize()) {
         // Añadir GoogleMap al layout
@@ -73,6 +96,26 @@ fun MapScreen() {
                     snippet = "Punto de interés"
                 )
             }
+
+            // Añadir polígonos
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
         }
     }
 }
